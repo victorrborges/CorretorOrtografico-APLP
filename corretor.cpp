29f1eb021c92dcb8e709765118f2b70f988ca06e
Adbys Vasconcelos <adbys.andrade@ccc.ufcg.edu.br>
2017-06-11 14:09:29 -0300

    recebimento de texto do usuario, split do texto, checagem se as palavras do texto estao no dicionario, alterado recebimento de palavras para o dicionario pois estava salvando o end-dicionario

--- corretor.cpp
+++ corretor.cpp
@@ -1,13 +1,16 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector <string> dicionario;
+vector <string> palavrasDoTexto;
 string texto;
 
 void setup() {
+	
 	cout << "" << endl
 			 << "CORRETOR ORTOGRÁFICO!" << endl
 			 << "" << endl
@@ -17,32 +20,94 @@ void setup() {
 	string entrada;
 	while (entrada != "end-dicionario") {
 		cin >> entrada;
+		cin.ignore();
 		dicionario.push_back(entrada);
-	}
+	} 
+	dicionario.pop_back();
 }
 
-void menuPrincipal() {
-	cout << "" << endl
-	     << "MENU PRINCIPAL" << endl
-			 << "" << endl
-	     << "Escolha uma Opção:" << endl
-			 << "1 - Corrigir texto" << endl
-			 << "2 - Finalizar o programa" << endl;
-	// Se 1, chamar corrigirTexto();
+void dividirTexto() {
+
+	string delimiter = " ";
+
+	size_t pos = 0;
+	string token;
+	while ((pos = texto.find(delimiter)) != std::string::npos) {
+    		token = texto.substr(0, pos);
+    		palavrasDoTexto.push_back(token);
+    		texto.erase(0, pos + delimiter.length());
+	}
+
+
 }
+
+
+void varrerPalavras() {
+
+	dividirTexto();
+
+	for(int i = 0; i < palavrasDoTexto.size(); i++) {
+		bool correta = false;
+			for(int j = 0; j < dicionario.size(); j++) {
+				if (palavrasDoTexto[i] == dicionario[j]) {
+					correta = true;
+					break;
+					
+				} 
+
+			}
+		if (correta == false) {
+			//Palavra nao esta correta, chamar o corrigirTexto
+
+		}
+	
+	}
+
+}
+
 
 void corrigirTexto() {
 	cout << "CORREÇÃO DE TEXTOS" << endl
 	     << "" << endl
 			 << "Digite o texto que você deseja corrigir: " << endl;
-	getLine(cin,texto);
+	//nao esta salvando a ultima palavra do texto	
+	getline (cin,texto);
 	// Chamar o varrerPalavras();
+	varrerPalavras();
 }
 
-void varrerPalavras() {
-	// Para cada palavra errada encontrada,
-	// chamar o menuCorrecao();
+void menuPrincipal() {
+	int option;
+
+	while(option != 2) {
+
+		cout << "" << endl
+	     	<< "MENU PRINCIPAL" << endl
+				 << "" << endl
+	     	<< "Escolha uma Opção:" << endl
+				 << "1 - Corrigir texto" << endl
+				 << "2 - Finalizar o programa" << endl;
+		cin >> option;
+		cin.ignore();
+	
+
+		switch (option) {
+	
+			case 1:
+				corrigirTexto();
+				break;
+			case 2:
+				return;
+				break; 
+			default:
+				cout << "Opção inválida" << endl;
+				break;
+	
+		}
+	}
+
 }
+
 
 void menuCorrecao() {
 	// O print deve adicionar a palavra errada referente
