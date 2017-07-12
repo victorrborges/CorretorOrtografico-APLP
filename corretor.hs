377c6f1915f8a44cea17bb7cd022c1d14e4ebcaf
Adbys Vasconcelos <adbys.andrade@ccc.ufcg.edu.br>
2017-07-11 21:41:17 -0300

    adicionando menus e salvando o texto a ser corrigido

--- corretor.hs
+++ corretor.hs
@@ -13,14 +13,36 @@ lerDicionario = do
 	contents <- readFile "dicionario.txt"
 	putStr contents
 
+salvarTexto :: IO()
+salvarTexto = do
+	texto <- getLine
+	writeFile "texto.txt" texto
 
 escreverDicionario :: String -> IO()
 escreverDicionario palavra = do
 	appendFile "dicionario.txt" palavra
 	appendFile "dicionario.txt" "\n"
 
+menuPrincipal :: String -> IO()
+menuPrincipal opcao
+	| opcao == "1" = do
+		putStrLn("recebe texto")
+		salvarTexto
+		opcao <- getLine
+		menuPrincipal opcao
+	| otherwise = putStr("")
+
+
+
 
 main = do
+	putStrLn "CORRETOR ORTOGRAFICO!\n"
+	putStrLn "Primeiramente, iremos configurar o dicionário.\nDigite as palavras que você deseja incluir no dicionário:\nobs: ao finalizar, digite \"end-dicionario\""
 	palavra <- getLine
 	menuDicionario palavra
 	lerDicionario
+	
+	putStrLn "MENU PRINCIPAL\nEscolha uma Opção:\n1 - Corrigir texto\n2 - Finalizar o programa"
+	
+	opcao <- getLine
+	menuPrincipal opcao
